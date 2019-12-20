0dd3f721a7821f2681026136ab4103102dc8d5e0
unknown <2934347631@qq.com>
2019-12-20 20:15:14 +0800

    '2019/12/20'

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  version: "7.3.0",
+  note: '模板消息调整为订阅消息',
+  subDomain: "tz", // 根据教程 https://www.yuque.com/apifm/doc/qr6l4m 查看你自己的 subDomain
+  appid: "wx67b189682f0cdbc0", // 您的小程序的appid，购物单功能需要使用
+  shareProfile: '百款精品商品，总有一款适合您', // 首页转发的时候话术
+  requireBindMobile: true, // 是否强制绑定手机号码才能使用
+  kanjiaRequirePlayAd: true // 是否必须要观看视频广告后才可以砍价
+}
